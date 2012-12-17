6da250c2f8696ebf713420311afe2923fde4d75e
sebastien LEVESQUE <slevesque@linagora.com>
2012-12-17 12:03:59 +0100

    foo

--- business/RestWebService/src/main/java/com/linagora/openpaas/backend/dao/MessageDAO.java
+++ business/RestWebService/src/main/java/com/linagora/openpaas/backend/dao/MessageDAO.java
@@ -3,11 +3,12 @@ package com.linagora.openpaas.backend.dao;
 import java.util.List;
 
 import com.linagora.openpaas.backend.dto.Message;
+import com.linagora.openpaas.backend.dto.User;
 
 public interface MessageDAO {
 
-	public List<Message> findAvailableMessage();
-	public void create(Message message);
-	public int getMessagesNumber();
+	public List<Message> findAvailableMessage(User user);
+	public int getMessagesNumber(User user);
+	public void create(Message message, User user);
 
 }
--- business/RestWebService/src/main/java/com/linagora/openpaas/backend/webservice/MessageService.java
+++ business/RestWebService/src/main/java/com/linagora/openpaas/backend/webservice/MessageService.java
@@ -13,6 +13,7 @@ import javax.ws.rs.core.MediaType;
 import com.linagora.openpaas.backend.dao.InMemoryMessageDAO;
 import com.linagora.openpaas.backend.dao.MessageDAO;
 import com.linagora.openpaas.backend.dto.Message;
+import com.linagora.openpaas.backend.dto.User;
 
 
 @Path("/messageService")
@@ -25,23 +26,42 @@ public class MessageService {
     	Message message = new Message();
     	message.setBody(body);
     	message.setSubject(subject);
-    	messageDAO.create(message);
+    	User u = getCurrentUser();
+    	messageDAO.create(message,u);
     }
 
     @GET
     @Path("/messages")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Message> getAvailableMessages() {
-        return  messageDAO.findAvailableMessage();
+    	User u = getCurrentUser();
+        return  messageDAO.findAvailableMessage(u);
     }
     
     @GET
     @Path("/messages/number")
     @Produces(MediaType.APPLICATION_JSON)
     public int getMessagesNumber() {
-        return  messageDAO.getMessagesNumber();
+    	User u = getCurrentUser();
+        return  messageDAO.getMessagesNumber(u);
     }
     
+    
+    
+    private static User myUser ;
+    
+    private User getCurrentUser(){
+    	
+    	if(myUser==null){
+	    	myUser = new User();
+	    	myUser.setFirstname("prenom1");
+	    	myUser.setLastname("nom1");
+	    	myUser.setMail("prenom1.nom1@openpaas.com");
+	    	myUser.setLogin("login1");
+    	}
+    	
+    	return  myUser;
+    }
     
     
 //    @PUT
